0da1d58809412032eab8fa0d826b6448ff0c01d3
Renato Gonçalves Dias <renatodiasbr@gmail.com>
2018-10-24 17:53:22 -0300

    ...

--- src/components/PostNew.jsx
+++ src/components/PostNew.jsx
@@ -1,10 +1,69 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostNew extends Component {
-  state = {};
+  renderInputText(field) {
+    return (
+      <div className="form-group row">
+        <label htmlFor={field.input.name} className="col-12 col-form-label">
+          {field.label}
+        </label>
+        <div className="col-12">
+          <input
+            {...field.input}
+            id={field.input.name}
+            type="text"
+            className="form-control"
+          />
+          {field.meta.error}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit = values => {
+    console.log(values);
+  };
+
   render() {
-    return <span>Post New.</span>;
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field name="title" label="Title:" component={this.renderInputText} />
+        <Field
+          name="categories"
+          label="Categories:"
+          component={this.renderInputText}
+        />
+        <Field
+          name="content"
+          label="Content:"
+          component={this.renderInputText}
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
 
-export default PostNew;
+function validate(values) {
+  let errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  return errors;
+}
+
+export default reduxForm({ validate, form: "PostNew" })(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import posts from "./posts";
 
-export default combineReducers({ posts });
+export default combineReducers({
+  posts,
+  form: formReducer
+});
